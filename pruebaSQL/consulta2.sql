0bb1c7d9299479a9dfea2f92eb29b438571ac668
Balmore Ramirez <camaleoncode@gmail.com>
2022-11-10 09:52:42 -0600

    sergun cosnulta de la suma

--- pruebaSQL/consulta2.sql
+++ pruebaSQL/consulta2.sql
@@ -1,11 +1,4 @@
-select 
-employees.first_name as nombreEmpleado, 
-employees.email_address as CorreoEmpleado, 
-orders.id as IdOrden,
-customers.last_name as NombreCliente,
-orders.order_date as FechaOrden,
-employees.id as IdEmpleado
-from orders
-inner join employees
-inner join customers
-where employees.id = 1 and  orders.order_date between '2006-04-01 00:00:00' and '2006-05-01 00:00:00'
+select sum(standard_cost) as TotalCostos, company
+from products
+         inner join suppliers
+where company = 'Supplier J'
